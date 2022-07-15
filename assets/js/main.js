f3642104d3afd60d458085c38bd77f0ef7b7c4d9
usablepaper <usablepaper@gmail.com>
2022-07-16 03:44:16 +0900

    home 페이이지 마크업, 스타일 구현

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,8 @@ const buttonCommunity = document.querySelector(".container-community");
 const listCommunity = document.querySelector(".list-community");
 const buttonMenu = document.querySelector(".button-menu");
 const itemsCommunity = document.querySelectorAll(".item-community");
-const navbar = document.querySelector(".header-nav");
+const menubar = document.querySelector(".menubar");
+const buttonHome = document.querySelector(".button-home");
 
 buttonCommunity.addEventListener("click", () => {
   listCommunity.classList.toggle("on");
@@ -16,11 +17,17 @@ buttonCommunity.addEventListener("focusout", () => {
 
 const before = [];
 window.addEventListener("scroll", (e) => {
-  before.pop() < window.scrollY && window.scrollY > 100
-    ? navbar.classList.add("off")
-    : navbar.classList.remove("off");
+  if (before.pop() < window.scrollY && window.scrollY > 100) {
+    menubar.classList.add("off");
+    buttonHome.classList.add("off");
+  } else {
+    menubar.classList.remove("off");
+    buttonHome.classList.remove("off");
+  }
   listCommunity.classList.contains("on") &&
     window.scrollY > 100 &&
     listCommunity.classList.remove("on");
   before.push(window.scrollY);
 });
+
+ScrollReveal().reveal("section", { distance: "100px", duration: 2000 });
