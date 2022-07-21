eb73773374c9c3229fd7e1fbbd0e47114fef8906
usablelpaper <usablepaper@gmail.com>
2022-07-22 03:21:22 +0900

    company 페이지 마크업 및 스타일구현, 반응형 이벤트 추가

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,60 @@
+// medium api 요청
 const url =
-  "https://cors-anywhere.herokuapp.com/https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@fameuniverse";
-const header = {
-  rss_url: "https://medium.com/feed/@fameuniverse",
-};
+  "https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@fameuniverse";
 
 // 최신 포스트 받아오기
-// async function getUser() {
-//   try {
-//     const res = await axios.get(url, {
-//       headers: header,
-//     });
+async function getUser() {
+  try {
+    const res = await axios.get(url, {});
+    let { title, pubDate, thumbnail, content, link } = res.data.items[0];
+    let contentLength = content.length;
+    const startContent = content.indexOf("<p>") + 3;
+    const stopContent = content.indexOf("</p>");
+    content = content.substring(startContent, stopContent);
 
-//     let { title, pubDate, thumbnail, content, link } = res.data.items[0];
-//     let contentLength = content.length;
-//     content = content.substring(
-//       content.indexOf("<p>" + 3),
-//       content.indexOf("</p>")
-//     );
-//     console.log(content);
-//   } catch (err) {
-//     console.error(err);
-//   }
-// }
-// getUser();
+    console.log("title:", title);
+    console.log("pubDate:", pubDate);
+    console.log("thumbnail:", thumbnail);
+    console.log("content:", content);
+    console.log("link:", link);
+  } catch (err) {
+    console.error(err);
+  }
+}
+getUser();
 
+// company 페이지 반응형 background video
+addEventListener("DOMContentLoaded", () => {
+  function makeVideoResponsible() {
+    const target = document.querySelector(".testvideo");
+    if (target) {
+      const currentRatio = innerWidth / innerHeight;
+      const videoRatio = 1.7777777777777777;
+      if (videoRatio > currentRatio) {
+        target.style.width = "auto";
+        target.style.height = "100vh";
+      } else {
+        target.style.height = "auto";
+        target.style.width = "100vw";
+      }
+    }
+  }
+
+  makeVideoResponsible();
+  addEventListener("resize", makeVideoResponsible);
+});
+
+// 네비게이션바 마우스 이벤트
+const linkMenu = document.querySelector(".container-link");
+const gray03 = "#5B5B5B";
+const navy = "#14003D";
+const primary = "#6B1FFF";
+const blue = "#30008F";
+linkMenu.addEventListener("mousedown", (e) => {
+  e.target.style.color = gray03;
+});
+
+// 네비게이션바 클릭 이벤트
 const buttonCommunity = document.querySelector(".container-community");
 const listCommunity = document.querySelector(".list-community");
 const buttonMenu = document.querySelector(".button-menu");
@@ -31,7 +62,7 @@ const itemsCommunity = document.querySelectorAll(".item-community");
 const menubar = document.querySelector(".menubar");
 const buttonHome = document.querySelector(".button-home");
 
-buttonCommunity.addEventListener("click", () => {
+buttonCommunity.addEventListener("click", (e) => {
   listCommunity.classList.toggle("on");
   buttonMenu.classList.toggle("on");
 });
@@ -41,6 +72,19 @@ buttonCommunity.addEventListener("focusout", () => {
   buttonMenu.classList.remove("on");
 });
 
+buttonCommunity.addEventListener("mousedown", () => {
+  buttonCommunity.style.backgroundColor = navy;
+});
+
+buttonCommunity.addEventListener("mouseup", () => {
+  buttonCommunity.style.backgroundColor = primary;
+});
+
+buttonCommunity.addEventListener("mouseover", () => {
+  buttonCommunity.style.backgroundColor = blue;
+});
+
+// 헤더 스크롤 반응형 이벤트
 const before = [];
 window.addEventListener("scroll", (e) => {
   if (before.pop() < window.scrollY && window.scrollY > 100) {
@@ -56,4 +100,5 @@ window.addEventListener("scroll", (e) => {
   before.push(window.scrollY);
 });
 
+// 섹션 반응형 이벤트
 ScrollReveal().reveal("section", { distance: "100px", duration: 2000 });
